747db8747720cad73ce417ac04b3da95960a84d8
Mohd Imran <mohd-i001@webreinvent.com>
2024-03-04 12:49:41 +0530

    Added spring back functionality
    - on pressing the spring back button the ball shows the spring behavior according to the parameters mass, stiffness, and damping.

--- lib/advancedtopics/friction_simulation/friction_simulation.dart
+++ lib/advancedtopics/friction_simulation/friction_simulation.dart
@@ -11,25 +11,47 @@ class FrictionSimulationPage extends StatefulWidget {
 class _FrictionSimulationState extends State<FrictionSimulationPage>
     with TickerProviderStateMixin {
   late AnimationController blockAnimationController;
-  double drag = 1;
-  double position = 1;
-  double velocity = 1;
+  double mass = 5;
+  double stiffness = 50;
+  double damping = 1;
+
+  late SpringSimulation displayedSpringSimulation;
+  bool isSpringing = false;
 
   @override
   void initState() {
     super.initState();
     blockAnimationController = AnimationController.unbounded(vsync: this);
+    _recalculateDisplayedSpringSimulation();
   }
 
   void _nudgeBlock() {
-    FrictionSimulation nonMovingSimulation =
-        FrictionSimulation(drag, position, velocity);
-    blockAnimationController.animateWith(nonMovingSimulation);
+    FrictionSimulation nudgeSimulation = FrictionSimulation(0.2, 0.0, 320);
+    blockAnimationController.animateWith(nudgeSimulation);
+    isSpringing = false;
   }
 
   void _resetBlock() {
     FrictionSimulation nonMovingSimulation = FrictionSimulation(0, 0, 0);
     blockAnimationController.animateWith(nonMovingSimulation);
+    isSpringing = false;
+  }
+
+  void _springBack() {
+    SpringDescription springDescription =
+        SpringDescription(mass: mass, stiffness: stiffness, damping: damping);
+    SpringSimulation springSim = SpringSimulation(
+        springDescription, blockAnimationController.value, 0, 0);
+    blockAnimationController.animateWith(springSim);
+    isSpringing = true;
+  }
+
+  void _recalculateDisplayedSpringSimulation(){
+    SpringDescription springDescription = SpringDescription(mass: mass, stiffness: stiffness, damping: damping);
+
+    setState(() {
+      displayedSpringSimulation = SpringSimulation(springDescription, 400, 0, 0);
+    });
   }
 
   @override
@@ -47,35 +69,53 @@ class _FrictionSimulationState extends State<FrictionSimulationPage>
               top: 0,
               child: Column(
                 children: [
-                  Slider(
-                    min: 0.0,
-                    max: 2,
-                    value: drag,
-                    onChanged: (val) {
-                      setState(() {
-                        drag = val;
-                      });
-                    },
+                  Row(
+                    children: [
+                      const Text('Mass     '),
+                      Slider(
+                        min: 0.0,
+                        max: 10,
+                        value: mass,
+                        onChanged: (val) {
+                          setState(() {
+                            mass = val;
+                          });
+                        },
+                      ),
+                      Text(mass.toStringAsFixed(2)),
+                    ],
                   ),
-                  Slider(
-                    min: 0.0,
-                    max: 10,
-                    value: position,
-                    onChanged: (val) {
-                      setState(() {
-                        position = val;
-                      });
-                    },
+                  Row(
+                    children: [
+                      const Text('Stiffness'),
+                      Slider(
+                        min: 0.0,
+                        max: 100,
+                        value: stiffness,
+                        onChanged: (val) {
+                          setState(() {
+                            stiffness = val;
+                          });
+                        },
+                      ),
+                      Text(stiffness.toStringAsFixed(2)),
+                    ],
                   ),
-                  Slider(
-                    min: 0.0,
-                    max: 1000,
-                    value: velocity,
-                    onChanged: (val) {
-                      setState(() {
-                        velocity = val;
-                      });
-                    },
+                  Row(
+                    children: [
+                      const Text('Damping'),
+                      Slider(
+                        min: 0.0,
+                        max: 4,
+                        value: damping,
+                        onChanged: (val) {
+                          setState(() {
+                            damping = val;
+                          });
+                        },
+                      ),
+                      Text(damping.toStringAsFixed(2)),
+                    ],
                   ),
                 ],
               )),
@@ -96,7 +136,10 @@ class _FrictionSimulationState extends State<FrictionSimulationPage>
                 height: 50,
                 bottom: size.height / 3,
                 left: size.width / 4 - 25 + blockAnimationController.value,
-                child: Container(color: Colors.red),
+                child: Container(decoration: const BoxDecoration(
+                    color: Colors.red,
+                  shape: BoxShape.circle
+                ),),
               );
             },
           ),
@@ -111,6 +154,9 @@ class _FrictionSimulationState extends State<FrictionSimulationPage>
                     const SizedBox(width: 20),
                     ElevatedButton(
                         onPressed: _resetBlock, child: const Text('RESET')),
+                    const SizedBox(width: 20),
+                    ElevatedButton(
+                        onPressed: _springBack, child: const Text('SPRING BACK')),
                   ],
                 ),
               ],
